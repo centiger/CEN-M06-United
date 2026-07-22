2e90dd5dfdba26941de99fccee0d49ffe64a78a3
centiger <centiger2@gmail.com>
2026-07-22 19:40:49 +0900

    Add files via upload

--- hubs/sw.js
+++ hubs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cen-united-kingdom-hubs-v3-layout';
+const CACHE='cen-united-kingdom-hubs-v4-bible-bridge';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
